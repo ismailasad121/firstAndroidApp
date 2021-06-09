8693655f22feed50c92a80b400aa8df2969c537b
ismailasad121 <bsef18a044@pucit.edu.pk>
2021-06-09 20:15:00 +0500

    first android app simple signup form

---
+++ first/app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.first;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void submit(View view) {
+        Button button=findViewById(R.id.button);
+        Intent intent=new Intent(MainActivity.this,thankyou.class);
+        startActivity(intent);
+    }
+}
